38ed2c8a8e39de14d8018b888b06c96956941e3e
Ankit <ankit705034@users.noreply.github.com>
2026-08-21 11:41:07 +0530

    Add backup restore for phone migration

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'radha-jap-v10';
+const CACHE = 'radha-jap-v11';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icon.svg', './radha-rani.jpg', './barsana.jpg'];
 self.addEventListener('install', (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS))));
 self.addEventListener('activate', (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())));
